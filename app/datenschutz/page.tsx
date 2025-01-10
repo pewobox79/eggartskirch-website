fb8062c690a8ea22fb5091f59fe33caecbab2c9c
aviareps_pwolf <pwolf@aviareps.com>
2025-01-10 15:26:10 +0100

    styling update

---
+++ app/datenschutz/page.tsx
@@ -0,0 +1,6 @@
+const Datenschutz =()=>{
+
+    return <h1>datenschutz</h1>
+}
+
+export default Datenschutz
---
+++ app/impressum/page.tsx
@@ -0,0 +1,6 @@
+const Imprint =()=>{
+
+    return <h1>imprint</h1>
+}
+
+export default Imprint

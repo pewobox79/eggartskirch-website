3ec422348b79925c342a590f039c75c4b2c1c950
aviareps_pwolf <pwolf@aviareps.com>
2025-01-10 15:22:43 +0100

    styling update

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 const Footer =()=>{
 
     const date = new Date();
     const year = date.getFullYear();
     return <footer>
-        <div className={"copyright"}><p className={"copyrightText"}>&copy; { year } ST. ISIDOR PFARRKIRCHE. All Rights Reserved.</p></div>
+        <div className={"copyright"}>
+
+            <Link href={"/impressum"}>Impressum</Link>
+
+            <p className={"copyrightText"}>&copy; { year } ST. ISIDOR PFARRKIRCHE. All Rights Reserved.</p>
+            <Link href={"/datenschutz"}>Datenschutzerklärung</Link></div>
 
     </footer>
 }

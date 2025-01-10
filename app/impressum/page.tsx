39c98420eaf5d90feba3ac9fae4547c91afe1684
aviareps_pwolf <pwolf@aviareps.com>
2025-01-10 16:15:18 +0100

    legal content updated - review needed

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -1,6 +1,30 @@
+import styles from '@/styles/Globals.module.css'
+
 const Imprint =()=>{
 
-    return <h1>imprint</h1>
+    return <div className={styles.bodyContentWrapper}>
+        <h1>Impressum</h1>
+
+        <p>Ulrike Mohr<br/>
+            Seelsorgeeinheit Ravensburg West: Pfarramt Dreifaltigkeit<br/>
+            Eggartskirch 6<br/>
+            88213 Ravensburg</p>
+
+        <h2>Kontakt</h2>
+        <p>Telefon: +49 (0) 751 791 24 30<br/>
+            E-Mail: info@segenskirche-eggartskirch.de</p>
+
+        <h2>Redaktionell verantwortlich</h2>
+        <p>Ulrike Mohr<br/>
+            ADDRESSE NOCH EINGEBEN</p>
+
+        <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+        <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.</p>
+
+        <p>Quelle: <a
+            href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a>
+        </p></div>
 }
 
 export default Imprint
